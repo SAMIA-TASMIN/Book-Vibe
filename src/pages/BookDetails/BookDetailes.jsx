cdb0c5a19362e802b9ea643d32cc29d8c210045b
unknown <novaira489@gmail.com>
2026-02-10 23:40:32 +0600

    commit finished

--- src/pages/BookDetails/BookDetailes.jsx
+++ src/pages/BookDetails/BookDetailes.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
 import { addToStoredDB } from '../../Utility/addToDB';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import { ToastContainer, toast } from 'react-toastify';
 
+const MySwal = withReactContent(Swal)
 
 const BookDetailes = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     const bookId = parseInt(id);
     const data = useLoaderData();
-    const singleBook = data.find(book=>book.bookId === bookId);
-    const {bookName,image} = singleBook;
-    const handleMarkAsRead = id =>{
+    const singleBook = data.find(book => book.bookId === bookId);
+    const { bookName, image } = singleBook;
+
+
+    const handleMarkAsRead = id => {
+
+        // MySwal.fire({
+        //     title: "Good job!",
+        //     text: "You clicked the button!",
+        //     icon: "success"
+        // });
+
+        toast("Wow so easy!");
         addToStoredDB(id);
 
     }
@@ -17,7 +31,8 @@ const BookDetailes = () => {
         <div className=' w-1/2 mx-auto'>
             <img className='w-48' src={image} alt="" />
             <h5>{bookName}</h5>
-            <button onClick={()=>handleMarkAsRead(id)} className="btn m-2">Mark as Read</button>
+            <ToastContainer />
+            <button onClick={() => handleMarkAsRead(id)} className="btn m-2">Mark as Read</button>
             <button className="btn btn-accent m-2">Add to Wishlist</button>
         </div>
     );
